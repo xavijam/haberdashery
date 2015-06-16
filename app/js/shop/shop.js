d007dec99ea9d0b3b4e2b6a2fbc9c4c86e9f7ad8
Javier Álvarez Medina <xavijam@gmail.com>
2015-06-17 00:18:50 +0200

    Fixed shop parallax image path

--- app/js/shop/shop.js
+++ app/js/shop/shop.js
@@ -18,7 +18,7 @@ $(function(){
       location_map.render();
 
       // Parallax
-      this.$('.parallax-window').parallax({ imageSrc: '/img/shop/shop.jpg' });
+      this.$('.parallax-window').parallax({ imageSrc: '/img/layout/shop.jpg' });
     }
 
   });

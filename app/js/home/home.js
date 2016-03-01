eb7c079ba9050f38ffc989812409018c07dbc74f
xavijam <xavijam@gmail.com>
2016-03-01 23:50:31 +0100

    Adding slider logo and new images

--- app/js/home/home.js
+++ app/js/home/home.js
@@ -10,9 +10,17 @@ $(function(){
 
     _initViews: function() {
       PageView.prototype._initViews.call(this);
-
       // Slider
       $('.js-slider').flickerplate();
+      this._startBoni();
+
+    },
+
+    _startBoni: function() {
+      for (var i = 0, l = 36; i < l; i++) {
+        var makeItVisible = parseInt(Math.random() * 2) ?  true : false;
+        $('.js-logo').append('<span class="Slider-logoBoni ' + (!makeItVisible ? 'is-hidden' : '' ) + '" style="transform: rotate(' + i * 10 + 'deg);"></span>');
+      }
     }
 
   });

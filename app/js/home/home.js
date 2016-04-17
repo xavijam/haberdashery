388f10d23308500da52dfc1cc241d936e5974620
xavijam <xavijam@gmail.com>
2016-04-17 17:08:27 +0200

    Improved code for creating boni

--- app/js/home/home.js
+++ app/js/home/home.js
@@ -1,25 +1,25 @@
-
 /**
  *  Home entry
  *
  */
 
-$(function(){
-
+$(function () {
   var Home = PageView.extend({
 
-    _initViews: function() {
+    _initViews: function () {
       PageView.prototype._initViews.call(this);
-      // Slider
       $('.js-slider').flickerplate();
       this._startBoni();
-
     },
 
-    _startBoni: function() {
+    _startBoni: function () {
       for (var i = 0, l = 36; i < l; i++) {
-        var makeItVisible = parseInt(Math.random() * 2) ?  true : false;
-        $('.js-logo').append('<span class="Slider-logoBoni ' + (!makeItVisible ? 'is-hidden' : '' ) + '" style="transform: rotate(' + i * 10 + 'deg);"></span>');
+        var makeItVisible = parseInt(Math.random() * 2) ? true : false;
+        $('.js-logo').append(
+          $('<span>').addClass(
+            'Slider-logoBoni ' + (!makeItVisible ? 'is-hidden' : '')
+          ).attr('style', 'transform: rotate(' + i * 10 + 'deg)')
+        );
       }
     }
 
